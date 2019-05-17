a1e3943ba4155ee545411968b11984af1b8a4b71
wimba juventio <wimbajuventio@yahoo.com>
2019-05-17 14:23:54 +0700

    Initial Commit

---
+++ Basic Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic Animation
+//
+//  Created by Wimba Juventio Chandra on 17/05/19.
+//  Copyright © 2019 Wimba Juventio Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
