48eb03d8decc420253ac7b1e1f5d7e65a70d40af
wimba juventio <wimbajuventio@yahoo.com>
2019-05-17 15:07:47 +0700

    animation

--- Basic Animation/ViewController.swift
+++ Basic Animation/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myObject: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        myObject.layer.cornerRadius = myObject.frame.width/2 
         // Do any additional setup after loading the view.
     }
-
-
+ 
+    @IBAction func actionButton(_ sender: Any)
+    {
+        UIView.animate(withDuration: 1)
+        {
+            self.myObject.frame = CGRect(x: 23, y: 100, width: 100, height: 100)
+        }
+    }
 }
 
